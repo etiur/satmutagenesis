b64545c1d2a9dd0670f0d7cc13b7b30ae9c3370f
etiur <ruite123@gmail.com>
2020-10-19 17:08:14 +0200

    launch_mutations updated

--- launch_mutations.py
+++ launch_mutations.py
@@ -17,7 +17,7 @@ def parse_args():
     return args.folder, args.chain, args.resname, args.atom1, args.atom2
 
 class CreateLaunchFiles():
-    def __init__(self, input_, chain, resname, atom1, atom2, cpus=24):
+    def __init__(self, input_, chain, resname, atom1, atom2, cpus=24, test=False):
         """
         input_: (str) PDB files path
         chain: (str) the chain ID where the ligand is located
@@ -32,59 +32,56 @@ class CreateLaunchFiles():
         self.atom1 = atom1
         self.atom2 = atom2
         self.cpus = cpus
+        self.test = test
         self.yaml = None
         self.slurm = None
 
-    def input_creation(self, yaml_name, test=False):
+    def input_creation(self, yaml_name):
         """ create the .yaml input files for PELE"""
         if not os.path.exists("yaml_files"):
             os.mkdir("yaml_files")
-        self.yaml = "input_files/{}.yaml".format(yaml_name)
+        self.yaml = "yaml_files/{}.yaml".format(yaml_name)
         with open(self.yaml, "w") as inp:
-            inp.write("system: '{}'".format(self.input))
-            inp.write("chain: '{}'".format(self.chain))
-            inp.write("resname: '{}'".format(self.resname))
-            inp.write("induced_fit_exhaustive: true")
-            inp.write("seed: 12345")
-            inp.write("cpus: {}".format(self.cpus))
-            inp.write("atom_dist:\n- '{}'\n- '{}'".format(self.atom1, self.atom2))
-            inp.write("skip_preprocess: true")
-            if test:
-                inp.write("test: true")
+            inp.write("system: '{}'\n".format(self.input))
+            inp.write("chain: '{}'\n".format(self.chain))
+            inp.write("resname: '{}'\n".format(self.resname))
+            inp.write("induced_fit_exhaustive: true\n")
+            inp.write("seed: 12345\n")
+            inp.write("cpus: {}\n".format(self.cpus))
+            inp.write("atom_dist:\n- '{}'\n- '{}'\n".format(self.atom1, self.atom2))
+            inp.write("skip_preprocess: true\n")
+            if self.test:
+                inp.write("test: true\n")
 
-
-
-    def slurm_creation(self, slurm_name, yaml_file, test=False):
+    def slurm_creation(self, slurm_name):
         """Creates the slurm runing files for PELE"""
         if not os.path.exists("slurm_files"):
             os.mkdir("slurm_files")
         self.slurm = "slurm_files/{}.sh".format(slurm_name)
         with open(self.slurm, "w") as slurm:
-            slurm.write("#!/bin/bash")
-            slurm.write("#SBATCH -J PELE")
-            slurm.write("#SBATCH --output=mpi_%j.out")
-            slurm.write("#SBATCH --error=mpi_%j.err")
-            slurm.write("#SBATCH --ntasks=50")
-            if test:
-                slurm.write("#SBATCH --qos=debug\n")
-            slurm.write('module purgeexport PELE="/gpfs/projects/bsc72/PELE++/mniv/V1.6.2-b1/"')
-            slurm.write('export SCHRODINGER="/gpfs/projects/bsc72/SCHRODINGER_ACADEMIC"')
-            slurm.write('export PATH=/gpfs/projects/bsc72/conda_envs/platform/1.5.1/bin:$PATH')
-            slurm.write('module load impi')
-            slurm.write('module load intel mkl impi gcc # 2> /dev/null')
-            slurm.write('module load boost/1.64.0')
-            slurm.write('/gpfs/projects/bsc72/conda_envs/platform/1.5.1/bin/python3.8 -m pele_platform.main {}'.format(yaml_file))
+            slurm.write("#!/bin/bash\n")
+            slurm.write("#SBATCH -J PELE\n")
+            slurm.write("#SBATCH --output=mpi_%j.out\n")
+            slurm.write("#SBATCH --error=mpi_%j.err\n")
+            slurm.write("#SBATCH --ntasks={}\n".format(self.cpus))
+            if self.test:
+                slurm.write("#SBATCH --qos=debug\n\n")
+            slurm.write('module purgeexport PELE="/gpfs/projects/bsc72/PELE++/mniv/V1.6.2-b1/"\n')
+            slurm.write('export SCHRODINGER="/gpfs/projects/bsc72/SCHRODINGER_ACADEMIC"\n')
+            slurm.write('export PATH=/gpfs/projects/bsc72/conda_envs/platform/1.5.1/bin:$PATH\n')
+            slurm.write('module load impi\n')
+            slurm.write('module load intel mkl impi gcc # 2> /dev/null\n')
+            slurm.write('module load boost/1.64.0\n')
+            slurm.write('/gpfs/projects/bsc72/conda_envs/platform/1.5.1/bin/python3.8 -m pele_platform.main {}\n'.format(self.yaml))
 
-
-
-def create_20sbatch(chain, resname, atom1, atom2, cpus=24, folder="pdb_files"):
+def create_20sbatch(chain, resname, atom1, atom2, cpus=24, folder="pdb_files", test=False):
     """
     creates for each of the mutants the yaml and slurm files
 
         chain: (str) the chain ID where the ligand is located
         resname: (str) the residue name of the ligand in the PDB
-        atom1: (str) atom of the residue to follow in this format --> chain ID:position:atom name
-        atom2: (str) atom of the ligand to follow in this format --> chain ID:position:atom name
+        atom1: (str) atom of the residue to follow  --> chain ID:position:atom name
+        atom2: (str) atom of the ligand to follow  --> chain ID:position:atom name
         cpus: (str or int) how many cpus do you want to use
 
     """
@@ -95,9 +92,10 @@ def create_20sbatch(chain, resname, atom1, atom2, cpus=24, folder="pdb_files"):
     slurm_files = []
     for file in glob.glob("{}/*.pdb".format(folder)):
         name = file.replace("{}/".format(folder), "")
-        run = CreateLaunchFiles(file, chain, resname, atom1, atom2, cpus)
+        name = name.replace(".pdb", "")
+        run = CreateLaunchFiles(file, chain, resname, atom1, atom2, cpus, test=test)
         run.input_creation(name)
-        run.slurm_creation(name, yaml_file=run.yaml)
+        run.slurm_creation(name)
         yaml_files.append(run.yaml)
         slurm_files.append(run.slurm)
 
