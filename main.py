3bac5f66b24f9f79eb0b8a02c9da61f53aed5751
etiur <ruite123@gmail.com>
2020-10-24 15:30:11 +0200

    submit function was created within main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 from mutate_pdb import generate_mutations
 from pele_files import create_20sbatch
+from subprocess import call
+import glob
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Generate the mutant PDB and the corresponding running files")
@@ -19,10 +21,16 @@ def parse_args():
     args = parser.parse_args()
     return args.input, args.position, args.chain, args.resname, args.atom1, args.atom2, args.cpus
 
+def submit(slurm_folder):
+    for file in glob.glob("{}/*".format(slurm_folder)):
+        call(["sbatch", "{}".format(file)])
+
+
 def main():
     input, position, chain, resname, atom1, atom2, cpus = parse_args()
     pdb_names = generate_mutations(input, position)
     yaml_files, slurm_files = create_20sbatch(chain, resname, atom1, atom2, cpus=cpus)
+    submit("slurm_files")
 
 if __name__ == "__main__":
     #Run this if this file is executed from command line but not if is imported as API
