aa33b8457923cd70cd263f599aaf4743ce730d0a
etiur <ruite123@gmail.com>
2020-11-11 16:41:34 +0100

    ·updates

--- analysis.py
+++ analysis.py
@@ -1,8 +1,21 @@
 from glob import glob
 import pandas as pd
 import seaborn as sns
+import argparse
+from os.path import basename
 
-class Analysis:
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Analyse the different PELE simulations and create plots")
+    # main required arguments
+    parser.add_argument("--pele", required=False, default="./",
+                        help="Include the folder path where the pele simulations are located")
+    args = parser.parse_args()
+
+    return args.pele
+
+
+class SimulationData:
     def __init__(self, folder):
         """folder(str): path to the simulation folder"""
         self.folder = folder
@@ -32,33 +45,51 @@ class Analysis:
     def set_distribution(self, num):
         self.distribution = self.distance - num
 
-def analyse_all(folders="PELE_*"):
+
+def analyse_all(folders="."):
     """folders (str): path to the different PELE simulation folders to be analyzed"""
-    dictio = {}
-    for folder in glob("{}".format(folders)):
-        simulation = Analysis(folder)
-        simulation.filtering()
-        dictio[folder[5:]] = simulation
+    data_dict = {}
+    for folder in glob("{}/PELE_*".format(folders)):
+        name = basename(folder)
+        data = SimulationData(folder)
+        data.filtering()
+        data_dict[name[5:]] = data
         if "original" in folder:
-            distance = simulation.distance
+            distance = data.distance
         else:
             distance = 3
 
-    plot_dictio = {}
-    for key, value in dictio.items():
+    plot_dict = {}
+    for key, value in data_dict.items():
         if "original" not in key:
             value.set_distribution(distance)
-            plot_dictio[key] = value.distribution
+            plot_dict[key] = value.distribution
 
-    plot_dataframe = pd.DataFrame(plot_dictio)
-    plot_dataframe.columns.name = "mutations"
+    plot_dataframe = pd.DataFrame(plot_dict)
 
-    return dictio, plot_dataframe
+    return data_dict, plot_dataframe
+
 
 def box_plot(dataframe):
     """dataframe (tabular data): Any tabular structure that corresponds to the wide-form data accepted by seaborn"""
+    sns.set(font_scale=0.7)
+    sns.set_style("white")
     ax = sns.catplot(data=dataframe, kind="box", palette="Set2")
     ax.set(xlabel="Mutations", ylabel="Distance", title="Distance variantion with respect to original")
-    ax.savefig("distance.pdf" )
+    ax.savefig("distance.png", dpi=2500)
 
     return ax
+
+
+def main():
+    """folders (str): path to the different PELE simulation folders"""
+    folder = parse_args()
+    data_dict, plot_dataframe = analyse_all(folder)
+    ax = box_plot(plot_dataframe)
+
+    return data_dict, plot_dataframe, ax
+
+
+if __name__ == "__main__":
+    # Run this if this file is executed from command line but not if is imported as API
+    main()
