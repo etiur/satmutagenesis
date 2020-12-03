865e888808e2c5ab6f6c36a4f0e2b5aabe8736e5
etiur <ruite123@gmail.com>
2020-12-03 11:25:46 +0100

    fixed errors

--- pele_files.py
+++ pele_files.py
@@ -150,11 +150,11 @@ class CreateLaunchFiles:
 
             lines2 = ['module purge\n',
                       'module load intel gcc openmpi/1.8.1 boost/1.63.0 python/2.7.3 MKL/11.3 GTK+3/3.2.4\n',
-                      'export PYTHONPATH=/gpfs/projects/bsc72/PELEPlatform/1.2.3/:$PYTHONPATH'
-                      'export PYTHONPATH=/gpfs/projects/bsc72/adaptiveSampling/bin_nord/v1.6.2/:$PYTHONPATH'
+                      'export PYTHONPATH=/gpfs/projects/bsc72/PELEPlatform/1.2.3/:$PYTHONPATH\n'
+                      'export PYTHONPATH=/gpfs/projects/bsc72/adaptiveSampling/bin_nord/v1.6.2/:$PYTHONPATH\n'
                       'export PYTHONPATH=/gpfs/projects/bsc72/PELEPlatform/external_deps/:$PYTHONPATH\n',
                       'export PYTHONPATH=/gpfs/projects/bsc72/lib/site-packages_mn3:$PYTHONPATH\n',
-                      'export MPLBACKEND=Agg\n', 'export OMPI_MCA_coll_hcoll_enable=0\n', 'export OMPI_MCA_mtl=^mxm'
+                      'export MPLBACKEND=Agg\n', 'export OMPI_MCA_coll_hcoll_enable=0\n', 'export OMPI_MCA_mtl=^mxm\n'
                       'python -m pele_platform.main {}\n'.format(self.yaml)]
 
             lines.extend(lines2)
