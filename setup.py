686c9619e7cd74e0267d201f3e37e3c960bb16ce
etiur <ruite123@gmail.com>
2021-01-15 12:49:16 +0100

    writing documentation

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(name="saturated_mutagenesis-etiur", author="Ruite Xiang", author_email="ruite.xiang@bsc.es",
       description="Study the effects of mutations on Protein-Ligand interactions",
       url="https://github.com/etiur/saturated_mutagenesis", license="MIT", version="0.0.1",
-      packages=find_packages(), python_requires=">=2.7", long_description=long_description,
+      packages=find_packages(), python_requires=(">=2.7", "<3.0"), long_description=long_description,
       long_description_content_type="test/markdown",
       classifiers=["Programming Language :: Python :: 2.7",
                    "License :: OSI Approved :: MIT License",
@@ -14,4 +14,7 @@ setup(name="saturated_mutagenesis-etiur", author="Ruite Xiang", author_email="ru
                    "Intended Audience :: Science/Research",
                    "Natural Language :: English",
                    "Environment :: Console",
-                   "Development Status :: 1 - Planning"])
+                   "Development Status :: 1 - Planning",
+                   "Topic :: Scientific/Engineering :: Bio-Informatics"],
+      install_requires=["pmx", "fpdf", "matplotlib", "numpy", "pandas", "seaborn"],
+      keywords=["protein engineering", "bioinformatics", "mutate proteins", "simulations"])
