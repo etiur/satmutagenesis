d3a548ca0d82cfe91ef93d2730e4467d4b1ec5c6
etiur <ruite123@gmail.com>
2021-01-15 18:34:50 +0100

    fixing errors

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    long_description = fh.read().decode("UTF-8")
 
 setup(name="saturated_mutagenesis-etiur", author="Ruite Xiang", author_email="ruite.xiang@bsc.es",
       description="Study the effects of mutations on Protein-Ligand interactions",
       url="https://github.com/etiur/saturated_mutagenesis", license="MIT", version="0.0.1",
-      packages=find_packages(), python_requires=(">=2.7", "<3.0"), long_description=long_description,
-      long_description_content_type="test/markdown",
+      packages=find_packages(), python_requires=">=2.7, <3.0", long_description=long_description,
+      long_description_content_type="text/markdown",
       classifiers=["Programming Language :: Python :: 2.7",
                    "License :: OSI Approved :: MIT License",
                    "Operating System :: Unix",
@@ -17,4 +17,4 @@ setup(name="saturated_mutagenesis-etiur", author="Ruite Xiang", author_email="ru
                    "Development Status :: 1 - Planning",
                    "Topic :: Scientific/Engineering :: Bio-Informatics"],
       install_requires=["pmx", "fpdf", "matplotlib", "numpy", "pandas", "seaborn"],
-      keywords=["protein engineering", "bioinformatics", "mutate proteins", "simulations"])
+      keywords="protein engineering, bioinformatics, mutate proteins, simulations")
