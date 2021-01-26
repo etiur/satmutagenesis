0af7fcdd388fb46ab1c550fb08af63c4b19cd243
etiur <ruite123@gmail.com>
2021-01-26 20:33:22 +0100

    the correct version

--- satumut/tests/test_simulation.py
+++ satumut/tests/test_simulation.py
@@ -4,6 +4,7 @@ This module provides test of the simulations.py's functions and classes
 from ..simulation import SimulationRunner
 from subprocess import Popen
 
+
 class TestSimulationRunner:
     """
     It is a class that tests the SimulationRunner class
@@ -13,7 +14,6 @@ class TestSimulationRunner:
         Test the submit function in SimulationRunner
         """
         simulation = SimulationRunner("data/test/PK2_F454T.pdb")
-        p = simulation.submit("data/test/test.yaml")
-        assert isinstance(simulation.command, list), "incorrect command format"
-        assert simulation.command[0] == "srun"
-        assert isinstance(p, Popen), "Submit isn't returning a subprocess.Popen object"
+        simulation.submit("data/test/test.yaml")
+        assert isinstance(simulation.commands, list), "incorrect command format"
+        assert isinstance(simulation.proc[0], Popen), "Submit isn't returning a subprocess.Popen object"
