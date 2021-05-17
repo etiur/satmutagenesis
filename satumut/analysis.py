ab155ed246a5884f58123f013d083021417537e0
etiur <ruite.xiang@gmail.com>
2021-05-17 15:47:39 +0200

    fixed errors

--- satumut/analysis.py
+++ satumut/analysis.py
@@ -703,7 +703,7 @@ def consecutive_analysis(file_name, wild=None, dpi=800, traj=10, output="summary
         pele_folders = commonlist(file_name)
     elif os.path.exists("{}".format(file_name)):
         folder, wild = find_log(file_name)
-        pele_folders = commonlist(file_name)
+        pele_folders = commonlist(folder)
     else:
         raise Exception("Pass a list of the path to the different folders")
 
--- satumut/rs_analysis.py
+++ satumut/rs_analysis.py
@@ -509,8 +509,8 @@ def consecutive_analysis_rs(file_name, dist1r, dist2r, dist1s, dist2s, wild=None
     if isiterable(file_name):
         pele_folders = commonlist(file_name)
     elif os.path.exists("{}".format(file_name)):
-        wild, folder = find_log(file_name)
-        pele_folders = commonlist(file_name)
+        folder, wild = find_log(file_name)
+        pele_folders = commonlist(folder)
     else:
         raise Exception("Pass a list of the path to the different folders")
 
