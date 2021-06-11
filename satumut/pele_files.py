80371fcf97ed1352caa942f845cd97b9476852c5
etiur <ruite.xiang@gmail.com>
2021-06-11 15:41:05 +0200

    changed the file liscence

--- satumut/pele_files.py
+++ satumut/pele_files.py
@@ -202,7 +202,7 @@ class CreateYamlFiles:
                 lines.append("usesrun: true\n")
             lines.append("working_folder: '{}'\n".format(folder))
             lines2 = ["cpus: {}\n".format(self.total_cpu), "cpus_per_mutation: {}\n".format(self.cpus),
-                      "pele_license: '/gpfs/projects/bsc72/PELE++/mniv/V1.7/license/'\n"]
+                      "pele_license: '/gpfs/projects/bsc72/PELE++/license'\n"]
             if not self.nord:
                 lines2.append("pele_exec: '/gpfs/projects/bsc72/PELE++/mniv/V1.7/bin/PELE-1.7_mpi'\n")
             else:
