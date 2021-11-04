b1b09845c9887d5163c5facfa8806c45fc53d959
jgilaber <joan.gilabert@bsc.es>
2021-11-04 11:21:21 +0100

    Fix bug crashing with no atom distances in analysis

--- satumut/analysis.py
+++ satumut/analysis.py
@@ -883,6 +883,8 @@ def consecutive_analysis(file_name, wild=None, dpi=800, traj=10, output="summary
     atoms: list[str]
         Series of atoms of the residues to follow in this format -> chain ID:position:atom name, multiple of 2
     """
+    if atoms is None:
+        atoms = []
     assert len(atoms) % 2 == 0, "The number of atoms to follow should be multiple of 2"
     if isiterable(file_name):
         pele_folders = commonlist(file_name)
